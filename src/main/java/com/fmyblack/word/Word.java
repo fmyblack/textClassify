df7f0d553482c72549b89fbb68b43f3e2d1f8010
fmyblack <fmyblack@gmail.com>
2017-02-17 19:42:33 +0800

    init

---
+++ src/main/java/com/fmyblack/word/Word.java
@@ -0,0 +1,21 @@
+package com.fmyblack.word;
+
+public class Word {
+
+	private int word_type;
+	private String word_name;
+	
+	public int getWord_type() {
+		return word_type;
+	}
+	public void setWord_type(int word_type) {
+		this.word_type = word_type;
+	}
+	public String getWord_name() {
+		return word_name;
+	}
+	public void setWord_name(String word_name) {
+		this.word_name = word_name;
+	}
+	
+}
